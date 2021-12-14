dd4ff466cdd6b9f90bedb11dc6d1ca26e4276c9f
Narong Saron <narong.saron@octoenergy.com>
2021-12-14 12:21:59 -0600

    Fix indentation

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,25 @@
 import NavBar from "./navbar";
 
 const layoutStyle = {
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-  width: "100%"
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%"
 };
 
 const contentStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column"
+    flex: 1,
+    display: "flex",
+    flexDirection: "column"
 };
 
 const Layout = props => (
-  <div className="Layout" style={layoutStyle}>
-    <NavBar />
-    <div className="Content container mx-auto p-6">
-      {props.children}
+    <div className="Layout" style={layoutStyle}>
+        <NavBar />
+        <div className="Content container mx-auto p-6">
+            {props.children}
+        </div>
     </div>
-  </div>
 );
 
 export default Layout;
