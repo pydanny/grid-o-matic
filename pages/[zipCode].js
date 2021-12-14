5aba1d8fb1883139ade454b9943941fab4aa24ce
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-12-14 07:29:48 -0800

    Get super simple product lookup working

--- pages/[zipCode].js
+++ pages/[zipCode].js
@@ -2,9 +2,35 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+import useSWR from 'swr'
+import { request, gql } from 'graphql-request'
+
+const url = 'https://api.oeus-kraken.energy/v1/graphql/'
+
+const fetcher = query => request(url, query)
+
+const queryGetAllAvailableProducts = `query getAllAvailableProducts {
+  products {
+    id
+    fullName
+    displayName
+    availableFrom
+    availableTo
+    availabilityStatus
+    term
+    isWholesale
+  }
+}`
+
+
 export default function Grid(){
   const router = useRouter()
   const { zipCode } = router.query  
+  const { data, error } = useSWR(
+    queryGetAllAvailableProducts,
+    fetcher
+  )
+  console.log(data)
   return (
     <div>
       <h1>Hello, World</h1>
