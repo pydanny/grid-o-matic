f6c3077721750fa8feeebd3e2eb8deafe1aad8d3
Narong Saron <narong.saron@octoenergy.com>
2021-12-14 09:35:07 -0600

    Add data map for table rendering

--- pages/the-grid.js
+++ pages/the-grid.js
@@ -1,7 +1,40 @@
 import styles from "../styles/TheGrid.module.css";
 
 export default function TheGrid() {
+    const fakeData = [
+        {
+            name: "Product 1",
+            price: "5",
+        },
+        {
+            name: "Product 2",
+            price: "2",
+        },
+        {
+            name: "Product 3",
+            price: "7",
+        }
+    ]
+
+    const tableRows = fakeData.map((product, index) => {
+        return(
+            <tr>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+            </tr>
+        )
+    })
+
+
     return(
-        <h1>Hello World!</h1>
+        <table className="table-auto hover:table-fixed">
+            <tbody>
+                <tr>
+                    <th>Product</th>
+                    <th>Pricing</th>
+                </tr>
+                {tableRows}
+            </tbody>
+        </table>
     )
-}
+    }
