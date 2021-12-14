9007315227d1e29f53e2cb30f6be89f7f49162c0
Narong Saron <narong.saron@octoenergy.com>
2021-12-14 10:03:21 -0600

    Add base page layout

---
+++ components/layout.js
@@ -0,0 +1,25 @@
+import NavBar from "./navbar";
+
+const layoutStyle = {
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  width: "100%"
+};
+
+const contentStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column"
+};
+
+const Layout = props => (
+  <div className="Layout" style={layoutStyle}>
+    <NavBar />
+    <div className="Content container mx-auto p-6">
+      {props.children}
+    </div>
+  </div>
+);
+
+export default Layout;
--- pages/the-grid.js
+++ pages/the-grid.js
@@ -1,4 +1,10 @@
-import styles from "../styles/TheGrid.module.css";
+import Layout from "../components/Layout";
+
+const tableStyle = {
+    height: "100%",
+    width: "100%",
+    border: "1px solid #ccc"
+};
 
 export default function TheGrid() {
     const fakeData = [
@@ -18,23 +24,25 @@ export default function TheGrid() {
 
     const tableRows = fakeData.map((product, index) => {
         return(
-            <tr>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
+            <tr key={index}>
+                <td className="px-4 py-3 border">{product.name}</td>
+                <td className="px-4 py-3 border">{product.price}</td>
             </tr>
         )
     })
 
 
     return(
-        <table className="table-auto hover:table-fixed">
-            <tbody>
-                <tr>
-                    <th>Product</th>
-                    <th>Pricing</th>
-                </tr>
-                {tableRows}
-            </tbody>
-        </table>
+        <Layout>
+            <table className="intertems-ce bg-transparent w-full border-collapse">
+                <tbody>
+                    <tr>
+                        <th className="px-4 py-3 border">Product</th>
+                        <th className="px-4 py-3 border">Pricing</th>
+                    </tr>
+                    {tableRows}
+                </tbody>
+            </table>
+        </Layout>
     )
-    }
+}
