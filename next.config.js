6bd23ef130c9e7f1f89136f19f6111e4181f08bb
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2022-09-02 08:35:38 -0700

    Hack to make it work

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   // basePath: "/octogrid",
-  assetPrefix: process.env.isProduction === "true" ? "/octogrid/" : "",
+  // assetPrefix: process.env.isProduction === "true" ? "/octogrid/" : "",
+  assetPrefix: process.env.isProduction === 'true' ? '' : '',
   env: {
-    isProduction: "false"
-  }
-}
+    isProduction: 'false',
+  },
+};
